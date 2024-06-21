764815d13ea4271c6cffc080eb4408afacc821b9
Koko-11 <idara.koko@gmail.com>
2024-06-21 15:12:38 -0700

    i

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Print without printf
+ * main - Entry point
  *
  * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 {
-	int i;
+	char r;
 
-	for (i = 0; i < 10; i++)
-	putchar(i);
+	for (r = 'z'; r >= 'a'; r--)
+	putchar(r);
 	putchar('\n');
 
 return (0);
