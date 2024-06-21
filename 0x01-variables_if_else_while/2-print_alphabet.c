04124c91fb75a0c5dbb1476f6e318520b7806d57
Koko-11 <idara.koko@gmail.com>
2024-06-21 13:55:31 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - everything in small letters
+ *
+ * Return always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+		return (0);
