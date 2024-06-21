fc62717bec1ce78273a0ef27468d3727a43ae4ae
Koko-11 <idara.koko@gmail.com>
2024-06-21 15:30:58 -0700

    p

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+	char dec;
+
+	for (x = 0; x > 10; x++)
+		putchar(x);
+
+	for (dec = 'a'; dec <= 'f'; dec++)
+		putchar(dec);
+		putchar('\n');
+
+	return (0);
+}
