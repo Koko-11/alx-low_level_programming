abd085604a2517122d95f7ecd7452ea2544d02cc
Koko-11 <idara.koko@gmail.com>
2024-06-22 15:35:37 -0700

    o

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <unistd.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: Prints a message to the standard error
- * Return: Always 1 (Error)
+ * Return: Always 1 (error)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	write(2, message, sizeof(message) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/**main - Empty point
+/**main - Entry point
  *
- * Return :0 (Success)
+ * Return: 0 (Success)
  *
  */
 
