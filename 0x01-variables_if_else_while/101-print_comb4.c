0eeedc3dee7ea0729486cd7165395e88b5572752
Koko-11 <idara.koko@gmail.com>
2024-06-22 16:32:39 -0700

    advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = i + 1; j < 9; j++)
+	for (i = 0; i < 8; i++)
+       	{
+		for (j = i + 1; j < 9; j++)
         {
-	for (k = j + 1; k < 10; k++)
+		for (k = j + 1; k < 10; k++)
             {
 		putchar(i + '0');
 		putchar(j + '0');
