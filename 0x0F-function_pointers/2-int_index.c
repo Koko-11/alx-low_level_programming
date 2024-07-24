5e0b7f4e93936eccf6e56a0980b956305f53abe5
Koko-11 <idara.koko@gmail.com>
2024-07-24 09:20:59 -0700

    c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
   * int_index - searches for an integer
   * @array: the array
   * @size: number of elements in the array 
-  * @cmp: a pointer to the function to be used to compare values
+  * @cmp: pointer to the function to compare values
   *
   * Return: -1
   */
@@ -15,16 +15,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size > 0)
 	{
-		if (array != NULL && cmp != NULL)
+	if (array != NULL && cmp != NULL)
+	{
+		while (i < size)
 		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
+		if (cmp(array[i]))
+			return (i);
 
-				i++;
-			}
+			i++;
 		}
+	}
 	}
 
 	return (-1);
